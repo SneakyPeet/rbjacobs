ca7709760b4e891af21d3e15218808cecf81e914
Pieter Koornhof <koornhof.pieter@gmail.com>
2019-04-12 09:19:18 +0200

    Complete

--- readme.md
+++ readme.md
@@ -1,19 +1,17 @@
-# Parse ofx files and send to ynab.
+# Download ofx files from FNB and send to YNAB
 
 ## Setup
 
-1. Get a developer token from ynab and set it as the `:ynab-token` property in config.edn.
-2. Run `(rb.core/get-budgets)` then copy the id for your budget to the config.edn files `:budget-id` property.
-3. Run `(rb.core/get-accounts)` and create an entry for each account you want to push transactions for in the `:accounts` list. 
-4. Match the account id to the account number from your bank account.
-
-## Usage with FNB Downloads
-1. Download .ofx transaction history files from the account views on the fnb website.
-2. Place these `zip` files under `resources/downloads`
-3. run `(rb.core/push-transactions-to-ynab)`
-
-## What If I want a different workflow?
-You are free to use the various functions in `rb.ofx` and `rb.core` to parse your .ofx or .zip files and send those transactions to ynab
+1. YNAB
+	1. Get a developer token from ynab and set it as the `:ynab-token` property in config.edn.
+	2. Run `(rb.core/get-budgets)` then copy the id for your budget to the config.edn files `:budget-id` property.
+	3. Run `(rb.core/get-accounts)` and create an entry for each account you want to push transactions for in the `:accounts` list. 
+	4. Match the account id to the account number from your bank account.
+2. FNB
+	1. Setup Chrome Driver (see below) 
+	2. Create a ReadOnly Login on FNB and add the login details to your config.edn
+3. Replace the code in `rb.core/-main` to point to your config
+4. Run your code (either `clj -m rb.core` or from the repl
 
 ## Auto downloading from fnb
 [etaoin](https://github.com/igrishaev/etaoin) is used for browser automation.
@@ -21,5 +19,3 @@ You are free to use the various functions in `rb.ofx` and `rb.core` to parse you
 Install Chrome Driver and make sure your chrome version matches. `brew cask install chromedriver`.
 
 If you want to use a different browser simply swap out the `driver` in `rb/fnb.clj` and see the etaoin website for install details.
-
-
--- src/rb/core.clj
+++ src/rb/core.clj
@@ -2,21 +2,24 @@
   (:require [rb.ynab :as ynab]
             [rb.ofx :as ofx]
             [rb.fnb :as fnb]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            [me.raynes.fs :as fs]))
 
 
-(defn get-config []
-  (-> (read-string (slurp "config.edn"))
-      (update :accounts #(->> %
-                              (map (juxt :bank-account-number identity))
-                              (into {})))))
+(defn get-config
+  ([] (get-config "config.edn"))
+  ([path]
+   (-> (read-string (slurp path))
+       (update :accounts #(->> %
+                               (map (juxt :bank-account-number identity))
+                               (into {}))))))
 
 
-(defn get-token [] (:ynab-token (get-config)))
+(defn get-token [config] (:ynab-token config))
 
 
-(defn get-client []
-  (ynab/client (get-token)
+(defn get-client [config]
+  (ynab/client (get-token config)
                (fn [request]
                  (-> request
                      (assoc :throw-exceptions false)
@@ -24,17 +27,11 @@
                      ynab/parse-response))))
 
 
-(defn get-config []
-  (-> (read-string (slurp "config.edn"))
-      (update :accounts #(->> %
-                              (map (juxt :bank-account-number identity))
-                              (into {})))))
-
 
 (defn get-budgets
   "returns a map with budget name as key and ynab budget id as value"
-  []
-  (let [client (get-client)]
+  [config]
+  (let [client (get-client config)]
     (->> ynab/budgets
          client
          ynab/->data
@@ -46,38 +43,36 @@
 (defn get-accounts
   "returns a map with account name as key and ynab account id as value.
    expects budget-id should be set in config"
-  ([] (get-accounts (get-config)))
-  ([config]
-   (let [client (get-client)
-         budget-id (:budget-id config)]
-     (->> (ynab/accounts budget-id)
-          client
-          ynab/->data
-          :accounts
-          (map (juxt :name :id))
-          (into {})))))
+  [config]
+  (let [client (get-client config)
+        budget-id (:budget-id config)]
+    (->> (ynab/accounts budget-id)
+         client
+         ynab/->data
+         :accounts
+         (map (juxt :name :id))
+         (into {}))))
 
 
 (defn process-ofx-zip-transactions
   "Parses all .ofx files contained in .zip files in the resources/downloads folder and returns a list of transactions"
-  ([] (process-ofx-zip-transactions (get-config)))
-  ([config]
-   (->> (ofx/process)
-        (map (fn [{:keys [amount date-posted id memo account-number]}]
-               (let [{:keys [account-id name]} (get-in config [:accounts account-number])]
-                 (assoc
-                  (ynab/->transaction account-id
-                                      (ynab/->date date-posted)
-                                      (ynab/->amount amount)
-                                      memo)
-                  :account-name name)))))))
+  [config]
+  (->> (ofx/process)
+       (map (fn [{:keys [amount date-posted id memo account-number]}]
+              (let [{:keys [account-id name]} (get-in config [:accounts account-number])]
+                (assoc
+                 (ynab/->transaction account-id
+                                     (ynab/->date date-posted)
+                                     (ynab/->amount amount)
+                                     memo)
+                 :account-name name))))))
 
 
 (defn push-transactions-to-ynab
-  ([] (push-transactions-to-ynab (get-client) (process-ofx-zip-transactions)))
-  ([transactions] (push-transactions-to-ynab (get-client) transactions))
-  ([client transactions]
-   (let [budget-id (:budget-id (get-config))]
+  ([config] (push-transactions-to-ynab (get-client config) (process-ofx-zip-transactions config)))
+  ([config transactions]
+   (let [client (get-client config)
+         budget-id (:budget-id config)]
      (for [[account-name transactions] (group-by :account-name transactions)]
        (let [response (->> (ynab/create-transactions budget-id transactions)
                            client
@@ -91,10 +86,28 @@
           (when error?
             {:error? (get-in response [:body :error])})))))))
 
+;;; CUSTOM
+
+
+(defn run-example []
+  (let [primary-account (get-config "primary.edn")
+        secondary-account (get-config "secondary.edn")
+        downloads (:downloads-folder primary-account)
+        full (update primary-account :accounts merge (:accounts secondary-account))]
+    (fs/delete-dir (fs/file downloads))
+    (fs/mkdir downloads)
+    (fnb/download-ofx primary-account)
+    (fnb/download-ofx secondary-account)
+    (->> (process-ofx-zip-transactions full)
+         (push-transactions-to-ynab full)
+         (#(do (prn %) %)))))
+
+
+(defn -main [& args]
+  (run-example))
+
 
 (comment
-  (->> (process-ofx-zip-transactions)
-       push-transactions-to-ynab)
 
   (fnb/download-ofx (get-config))
   )
